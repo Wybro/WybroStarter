cd107fdb397bcb74e88c917e48c06157c30976f4
Connor <connor@wybro.com>
2018-08-05 10:53:38 -0700

    Add extra UIView centering function

--- WybroStarter/UIView+Extensions.swift
+++ WybroStarter/UIView+Extensions.swift
@@ -14,14 +14,28 @@ public extension UIView {
         case vertical, horizontal
     }
     
-    public func center(in superview: UIView, type: LayoutType) -> NSLayoutConstraint {
+    public func center(in superview: UIView,
+                       type: LayoutType,
+                       offset: CGFloat = 0) -> NSLayoutConstraint {
         return NSLayoutConstraint(item: self,
                                   attribute: (type == .horizontal) ? .centerX : .centerY,
                                   relatedBy: .equal,
                                   toItem: superview,
                                   attribute: (type == .horizontal) ? .centerX : .centerY,
                                   multiplier: 1,
-                                  constant: 0)
+                                  constant: offset)
+    }
+    
+    /**
+     Centers the view in the provided superview vertically and horizontally,
+     with optional offset.
+    */
+    public func center(in superview: UIView,
+                       xOffset: CGFloat = 0,
+                       yOffset: CGFloat = 0) -> [NSLayoutConstraint] {
+        let vertical = center(in: superview, type: .vertical, offset: yOffset)
+        let horizontal = center(in: superview, type: .horizontal, offset: xOffset)
+        return [vertical, horizontal]
     }
     
     public func usingConstraints() -> UIView {

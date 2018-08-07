73885f99b4dfcefe211496824257347d7e4d7da6
Connor <connor@wybro.com>
2018-08-06 22:05:48 -0700

    Add support for matching edges of superview

--- WybroStarter/UIView+Extensions.swift
+++ WybroStarter/UIView+Extensions.swift
@@ -42,4 +42,32 @@ public extension UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         return self
     }
+    
+    public enum FillType {
+        case vertical, horizontal
+    }
+    
+    public func fill(_ direction: FillType, in superview: UIView) -> [NSLayoutConstraint] {
+        let first = NSLayoutConstraint(item: self,
+                                       attribute: direction == .vertical ? .top : .leading,
+                                       relatedBy: .equal,
+                                       toItem: superview,
+                                       attribute: direction == .vertical ? .top : .leading,
+                                       multiplier: 1,
+                                       constant: 0)
+        let second = NSLayoutConstraint(item: self,
+                                       attribute: direction == .vertical ? .bottom : .trailing,
+                                       relatedBy: .equal,
+                                       toItem: superview,
+                                       attribute: direction == .vertical ? .bottom : .trailing,
+                                       multiplier: 1,
+                                       constant: 0)
+        return [first, second]
+    }
+    
+    public func fill(in superview: UIView) -> [NSLayoutConstraint] {
+        let vertical = self.fill(.vertical, in: superview)
+        let horizontal = self.fill(.horizontal, in: superview)
+        return [vertical, horizontal].flatMap { $0 }
+    }
 }

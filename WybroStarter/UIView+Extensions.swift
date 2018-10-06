d78f099935f7f99c6523387b37bfc633b6fde399
Kyle <kyle@wybro.com>
2018-10-06 16:26:44 -0400

    Add comments for `addSubviewForLayout` and `addSubviewsForLayout`

--- WybroStarter/UIView+Extensions.swift
+++ WybroStarter/UIView+Extensions.swift
@@ -16,16 +16,22 @@ public extension UIView {
     
     /**
      Prepares the receiver to use Auto Layout (sets `translatesAutoresizingMaskIntoConstraints` to **false**).
-    */
+     */
     public func usingConstraints() -> UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         return self
     }
     
+    /**
+     Adds a provided view to the receiver's subviews using constraints
+     */
     public func addSubviewForLayout(_ view: UIView) {
         self.addSubview(view.usingConstraints())
     }
     
+    /**
+     Adds the provided views to the receiver's subviews using constraints
+     */
     public func addSubviewsForLayout(_ views: [UIView]) {
         views.forEach { addSubviewForLayout($0) }
     }
@@ -33,7 +39,7 @@ public extension UIView {
     /**
      Centers the view in the provided superview vertically or horizontally,
      with optional offset.
-    */
+     */
     public func center(in superview: UIView,
                        type: LayoutType,
                        offset: CGFloat = 0) -> NSLayoutConstraint {
@@ -45,11 +51,11 @@ public extension UIView {
                                   multiplier: 1,
                                   constant: offset)
     }
-
+    
     /**
      Centers the view in the provided superview vertically and horizontally,
      with optional offset.
-    */
+     */
     public func center(in superview: UIView,
                        xOffset: CGFloat = 0,
                        yOffset: CGFloat = 0) -> [NSLayoutConstraint] {
@@ -57,7 +63,7 @@ public extension UIView {
         let horizontal = center(in: superview, type: .horizontal, offset: xOffset)
         return [vertical, horizontal]
     }
-
+    
     /**
      Fills the view in the provided superview vertically or horizontally,
      with optional padding on each side.
@@ -71,15 +77,15 @@ public extension UIView {
                                        multiplier: 1,
                                        constant: padding)
         let second = NSLayoutConstraint(item: self,
-                                       attribute: direction == .vertical ? .bottom : .trailing,
-                                       relatedBy: .equal,
-                                       toItem: superview,
-                                       attribute: direction == .vertical ? .bottom : .trailing,
-                                       multiplier: 1,
-                                       constant: -padding)
+                                        attribute: direction == .vertical ? .bottom : .trailing,
+                                        relatedBy: .equal,
+                                        toItem: superview,
+                                        attribute: direction == .vertical ? .bottom : .trailing,
+                                        multiplier: 1,
+                                        constant: -padding)
         return [first, second]
     }
-
+    
     /**
      Fills the view in the provided superview vertically and horizontally,
      with optional padding on each side.
@@ -96,17 +102,17 @@ public extension Array where Element == UIView {
     public func fill(_ direction: LayoutType, in superview: UIView, padding: CGFloat = 0) -> [NSLayoutConstraint] {
         return self.map { $0.fill(direction, in: superview, padding: padding) }.flatMap { $0 }
     }
-
+    
     public func fill(in superview: UIView, padding: CGFloat = 0) -> [NSLayoutConstraint] {
         return self.map { $0.fill(in: superview, padding: padding) }.flatMap { $0 }
     }
-        
+    
     public func center(in superview: UIView,
                        type: LayoutType,
                        offset: CGFloat = 0) -> [NSLayoutConstraint] {
         return self.map { $0.center(in: superview, type: type, offset: offset) }
     }
-
+    
     public func center(in superview: UIView,
                        xOffset: CGFloat = 0,
                        yOffset: CGFloat = 0) -> [NSLayoutConstraint] {
